aefe89b10d6430b7e253aef949f12a9031992359
Umesh Chaurasiya <chaurasiyaumesh9@gmail.com>
2015-12-23 11:19:56 -0800

    my second commit

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ var express = require('express');
 var app = express();
 var sql = require('mysql');
 
-var connection = sql.createConnection({
+var pool = sql.createPool({
 	host: 'localhost',
 	user: 'root',
 	password: '',
 	database: 'classicmodels'
 });
-connection.connect(function(err){
+
+/*connection.connect(function(err){
 	if (!err)
 	{
 		console.log('DB connected successfully!');
 	}else{
 		console.log('Error connecting DB...');
 	}
-});
+});*/
 
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
@@ -30,19 +31,41 @@ app.get('/customers',function(request, response){
 	fetchCustomers(request, response);
 });
 
-function fetchCustomers(req, res){
+app.get('/customer/:id', function(request, response){
+ 	fetchCustomerById( request, response)
+});
 
-	connection.query('select * from customers',function(err, rows, fields){
-		connection.end();
-		if (!err)
-		{
-			//console.log('Got the result..listing now...',rows);
-			res.render('customers',{list: rows});
-		}else{
-			console.log('Error while performing the query..');
-		}
+
+function fetchCustomers(req, res){
+	pool.getConnection( function(err, conn){
+		conn.query("select * from customers", function(err, rows) {
+             if (!err)
+			{
+				res.render('customers',{list: rows});
+			}else{
+				console.log('Error while performing the query..check function fetchCustomers() for more details..');
+			}
+         })
 	});
+}
+
+function fetchCustomerById(req, res){
+	if ( req.params.id ){
+		pool.getConnection( function(err, conn){
+			conn.query('select * from customers where customerNumber="' + req.params.id +'"', function(err, row) {
+				 if (!err)
+				{
+					//res.send( row[0] ); 
+					res.render('customer',{ row: row[0]}); //getting only one records for specific id. thus accessing it via 0th index.
+				}else{
+					console.log('Error while performing the query..check function fetchCustomerById() for more details..');
+				}
+			 })
+		});
+	}
 	
 }
 
 app.listen(3030);
+
+//connection.end();
